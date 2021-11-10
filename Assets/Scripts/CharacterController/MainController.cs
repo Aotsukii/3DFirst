aee8e7d49422da6e2992d274a448942cd9c0715c
Elie Gazel <elie@gazel.net>
2021-11-10 13:58:26 +0100

    Updated bullet prefab + debug velocity display

--- Assets/Scripts/CharacterController/MainController.cs
+++ Assets/Scripts/CharacterController/MainController.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using System;
 
 [RequireComponent(typeof(CharacterController))]
 
@@ -18,6 +20,7 @@ public class MainController : MonoBehaviour
     public Vector3 moveDirection = Vector3.zero;
     float rotationX = 0;
 
+    public TextMeshProUGUI velocity;
     [HideInInspector]
     public bool canMove = true;
 
@@ -33,6 +36,10 @@ public class MainController : MonoBehaviour
         Vector3 forward = transform.TransformDirection(Vector3.forward);
         Vector3 right = transform.TransformDirection(Vector3.right);
 
+        if (velocity != null)
+        {
+            velocity.SetText("Player velocity : " + CalculVelocity());
+        }
         bool isRunning = Input.GetKey(KeyCode.LeftShift);
         float curSpeedX = canMove ? (isRunning ? (walkSpeed * runMultiplier) : walkSpeed) * Input.GetAxis("Vertical") : 0;
         float curSpeedY = canMove ? (isRunning ? (walkSpeed * runMultiplier) : walkSpeed) * Input.GetAxis("Horizontal") : 0;
@@ -62,5 +69,20 @@ public class MainController : MonoBehaviour
             playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
             transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
         }
+    }
+
+    private float CalculVelocity()
+    {
+        Vector3 horizontalVelocity = characterController.velocity;
+        horizontalVelocity = new Vector3(characterController.velocity.x, 0, characterController.velocity.z);
+
+        // The speed on the x-z plane ignoring any speed
+        float horizontalSpeed = horizontalVelocity.magnitude;
+        // The speed from gravity or jumping
+        float verticalSpeed = characterController.velocity.y;
+        // The overall speed
+        float overallSpeed = characterController.velocity.magnitude;
+
+        return ((float)Math.Round(overallSpeed * 100f) / 100f);
     }
 }
